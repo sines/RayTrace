5ab04c661b2e4c7d9d02138f7a32f965ff81a3ab
liuhua <liuhua@misrobot.com>
2016-03-14 22:10:06 +0800

    CMAKE

---
+++ cmake_x86.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+ECHO ############################################################
+ECHO #	Generating Visual Studio Project Files
+ECHO ############################################################
+
+SET SDK_DIR=D:\
+SET QT_ROOT=%SDK_DIR%Qt\Qt5.5.0\5.5\msvc2013
+
+rmdir /S /Q build_x86_vc120
+mkdir build_x86_vc120
+cd build_x86_vc120
+cmake.exe -L -DX86:BOOL=ON -DX64:BOOL=OFF -DQT_ROOT:PATH=%QT_ROOT% ../ -G"Visual Studio 12 2013"
+
+ECHO ############################################################
+ECHO #	Finished!
+ECHO ############################################################
+
+pause

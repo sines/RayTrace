d6ae7614030f53dae3a3ad8738853b488090fb85
liuhua <liuhua@misrobot.com>
2016-03-15 23:21:09 +0800

    cmakelist

--- cmake_x86.bat
+++ cmake_x86.bat
@@ -9,7 +9,7 @@ SET QT_ROOT=%SDK_DIR%Qt\Qt5.5.0\5.5\msvc2013
 rmdir /S /Q build_x86_vc120
 mkdir build_x86_vc120
 cd build_x86_vc120
-cmake.exe -L -DX86:BOOL=ON -DX64:BOOL=OFF -DQT_ROOT:PATH=%QT_ROOT% ../ -G"Visual Studio 12 2013"
+cmake.exe -L -DX86:BOOL=ON -DX64:BOOL=OFF  -DQT_ROOT:PATH=%QT_ROOT% ../ -G"Visual Studio 12 2013"
 
 ECHO ############################################################
 ECHO #	Finished!

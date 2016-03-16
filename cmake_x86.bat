54a92562cc6bd714b4a1e40df8b014566fcea6ab
liuhua <liuhua@misrobot.com>
2016-03-16 16:39:08 +0800

    整理RayTrace

--- cmake_x86.bat
+++ cmake_x86.bat
@@ -5,7 +5,6 @@ ECHO ############################################################
 
 SET SDK_DIR=D:\
 SET QT_ROOT=%SDK_DIR%Qt\Qt5.5.0\5.5\msvc2013
-
 rmdir /S /Q build_x86_vc120
 mkdir build_x86_vc120
 cd build_x86_vc120
